bde91ca178b154a941d561204ab67df4cd7a48bd
jjrose176@gmail.com <jjrose176@gmail.com>
2025-02-05 09:45:57 +0000

    names.py

---
+++ simpple_message.py
@@ -0,0 +1,25 @@
+
+
+ciggerete = "SNOOP DOGG"
+print(ciggerete.upper())
+
+sefirot= "keter"
+path = "tet"
+
+gateway= f"{sefirot.title()} {path.upper()}"
+print(gateway)
+print(f" hello, {gateway.title()}!")
+
+message = f" hello, {gateway}"
+print(message)
+
+hello =f" hello {message}"
+print(f"\n\t{hello}")
+
+
+
+india = "'abyss '" 
+india = india.replace("s '", "s'")
+print(india)
+
+
--- week1.py
+++ week1.py
@@ -1,3 +1,11 @@
-print("hello world")
+
+
+sefirot = ["Keter", "Hochma", "Bina", "Hesed", "Gevura", "Tiferet", "Netzah", "Hod", "Yesod", "Malkhut"]
 for x in range(10):
     print("Hello world!" + str(x))
+    for y in range(2):
+        print(sefirot[2] +""+ str(y))
+
+message = "hello world"
+print(mesage)
+
